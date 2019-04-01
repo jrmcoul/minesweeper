8b1f65190ea2123f5c01681a9efe5a538eea7668
Jeffrey March <jrmcoul@gmail.com>
2019-04-01 15:43:09 -0700

    async bug

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 
 function SelectDifficulty(props) {
   return (
-    <select onChange={props.onChange}>
+    <select onChange={props.onChange} className={props.className}>
       <option value="beginner">Beginner</option>
       <option value="intermediate">Intermediate</option>
       <option value="expert">Expert</option>
@@ -15,6 +15,12 @@ function SelectDifficulty(props) {
 function NewGame(props) {
   return (
     <button onClick={props.onClick} className={props.className}></button>
+  )
+}
+
+function SolveButton(props) {
+  return (
+    <button onClick={props.onClick} className ={props.className}>Solve</button>
   )
 }
 
@@ -88,7 +94,8 @@ class Game extends React.Component {
       gameOver: false,
       minesRemaining: this.boardSize.beginner[2],
       squaresRemaining: this.boardSize.beginner[0]*this.boardSize.beginner[1] - this.boardSize.beginner[2],
-		};
+		  border: [],
+    };
 
     this.convert1Dto2D = this.convert1Dto2D.bind(this);
     this.convert2Dto1D = this.convert2Dto1D.bind(this);
@@ -106,6 +113,9 @@ class Game extends React.Component {
     this.revealEmpty = this.revealEmpty.bind(this);
     this.checkSquaresRemaining = this.checkSquaresRemaining.bind(this);
     this.revealAllMines = this.revealAllMines.bind(this);
+    this.winLoss = this.winLoss.bind(this);
+    this.isBorder = this.isBorder.bind(this);
+    this.cullBorder = this.cullBorder.bind(this);
   }
 
   convert1Dto2D(index){
@@ -134,57 +144,59 @@ class Game extends React.Component {
     clearInterval(this.timer);
   }
 
-	handleClick(i) {
+	handleClick(i, isFirstClick) {
     const indices = this.convert1Dto2D(i);
   	const squares = this.state.squares.slice();
 
+
+    // Doesn't allow click of flagged square or when game is over 
   	if(squares[indices[0]][indices[1]].flagged || this.state.gameOver){
   		return;
   	}
 
-    if(squares[indices[0]][indices[1]].className === "square revealed" && 
+    // Initializing game on first move
+    if(this.state.move === 0) {
+      const mines = this.randomizeMines(this.state.height, this.state.width, [indices[0], indices[1]])
+      this.setBoard(mines);
+      this.startTimer();
+    }
+
+    // If clicked square is unrevealed 
+    if(squares[indices[0]][indices[1]].className === "square") {
+
+      squares[indices[0]][indices[1]].className = "square revealed";
+      const totalSquares = this.state.height*this.state.width - this.state.numMines;
+      const squaresRevealed = this.checkSquaresRemaining(squares);
+      const borderInd = (this.isBorder(indices[0],indices[1])) ? [indices] : null;
+
+      this.setState((state,props) => ({
+        move: state.move + 1,
+        squares: squares,
+        border: state.border.concat([borderInd]),
+        squaresRemaining: totalSquares - squaresRevealed,
+      }), () => {
+        if(squares[indices[0]][indices[1]].value === null) {
+          this.revealEmpty(indices);
+        }
+        this.winLoss(indices);
+        if(isFirstClick){
+          console.log(this.state.border);
+
+          this.setState({
+            border: this.cullBorder(this.state.border),
+          });
+        }
+      });
+
+    }
+    // If user clicks on revealed square that is surrounded by square.value flags
+    else if(squares[indices[0]][indices[1]].className === "square revealed" && 
         squares[indices[0]][indices[1]].value !== null) {
       if(squares[indices[0]][indices[1]].value === this.checkSurrounding(indices[0],indices[1],true)){
         this.clearSurrounding(indices[0],indices[1]);
       }
     }
 
-
-    let move = this.state.move;
-    if(move === 0) {
-      const mines = this.randomizeMines(this.state.height, this.state.width, [indices[0], indices[1]])
-      this.setBoard(mines);
-      this.startTimer();
-    }
-    squares[indices[0]][indices[1]].className = "square revealed";
-    move++;
-
-  	this.setState({
-      move: move,
-  		squares: squares,
-  	}, function(){
-      this.revealEmpty(indices);
-    });
-
-    if(squares[indices[0]][indices[1]].value === "M") {
-      this.stoptimer();
-      this.setState({
-        gameOver: true,
-      })
-      this.revealAllMines();
-      return;
-    }
-
-    const totalSquares = this.state.height*this.state.width - this.state.numMines;
-    const squaresRevealed = this.checkSquaresRemaining();
-    const gameOver = (totalSquares === squaresRevealed ? true : this.state.gameOver)
-    if(gameOver){
-      this.stopTimer();
-    }
-    this.setState({
-      squaresRemaining: totalSquares - squaresRevealed,
-      gameOver: gameOver
-    });
   }
 
   handleContextMenu(event, i) {
@@ -207,15 +219,14 @@ class Game extends React.Component {
       squares[indices[0]][indices[1]].className = "square flagged" :
       squares[indices[0]][indices[1]].className = "square"
 
-
-    
     this.setState({
       squares: squares,
       minesRemaining: minesRemaining,
     });
   }
 
-
+  // This is the event handler for the SelectDifficulty drop-down menu. It changes
+  // the difficulty and re-initializes all of the necessary parameters.
   handleDifficultyChange(event) {
     let settings = this.boardSize[event.target.value];
     this.stopTimer();
@@ -230,9 +241,12 @@ class Game extends React.Component {
       gameOver: false,
       minesRemaining: settings[2],
       squaresRemaining: settings[0]*settings[1] - settings[2],
+      border: [],
     });
   }
 
+  // This is the event handler for the NewGame button. It re-initializes all of
+  // the necessary parameters
   handleNewGameClick(event) {
     this.stopTimer();
     this.setState({
@@ -242,9 +256,12 @@ class Game extends React.Component {
       gameOver: false,
       minesRemaining: this.state.numMines,
       squaresRemaining: this.state.height*this.state.width - this.state.numMines,
+      border: [],
     });
   }
 
+  // This initializes the board of a given height and width by returning a 2x2
+  // array of squares objects
   initializeBoard(height, width) {
     const arr = new Array(height);
     for(let i = 0; i < height; i++) {
@@ -261,6 +278,9 @@ class Game extends React.Component {
     return arr;
   }
 
+  // This returns the indices of all the mines for initial board placement.
+  // "Lucky click" is instituted, meaning that the first click will always be
+  // a blank square
   randomizeMines(height, width, firstClick) {
     let arr = [];
     let iClick = firstClick[0];
@@ -288,6 +308,8 @@ class Game extends React.Component {
     return arr;
   }
 
+  // After mines have been randomized, this places all the mines and then
+  // calculates and places all of the numbered squares
   setBoard(arr){
     let squares = this.state.squares.slice();
     for(let ind = 0; ind < arr.length; ind++) {
@@ -308,14 +330,18 @@ class Game extends React.Component {
     })
   }
 
-  checkSurrounding(row,col,isClearAll) {
+  // Checks each suqare for either:
+  // -> the number of surrounding flags (isCountingFlags === true), or
+  // -> the number of surrounding mines (isCountingFlags === false).
+  // This returns the total number of surrounding flags or mines
+  checkSurrounding(row,col,isCountingFlags) {
     let mineCount = 0;
     const squares = this.state.squares.slice();
     for(let i = row - 1; i <=  row + 1; i++) {
-      for(let j = col -1; j <= col + 1; j++) {
+      for(let j = col - 1; j <= col + 1; j++) {
         if(i >= 0 && i < this.state.height &&
           j >= 0 && j < this.state.width){
-          if(isClearAll){
+          if(isCountingFlags){
             if(squares[i][j].flagged){
               mineCount++;
             }
@@ -330,15 +356,16 @@ class Game extends React.Component {
     return mineCount;
   }
 
-
+  // When the user clicks on a square surrounded by square.value flags,
+  // this clicks the remaining unrevealed squares
   clearSurrounding(row,col) {
     const squares = this.state.squares.slice();
     for(let i = row - 1; i <=  row + 1; i++) {
-      for(let j = col -1; j <= col + 1; j++) {
+      for(let j = col - 1; j <= col + 1; j++) {
         if(i >= 0 && i < this.state.height &&
           j >= 0 && j < this.state.width &&
           squares[i][j].className === "square"){
-          this.handleClick(this.convert2Dto1D([i,j]))
+          this.handleClick(this.convert2Dto1D([i,j]),false)
         }
       }
     }
@@ -348,23 +375,22 @@ class Game extends React.Component {
     })
   }
 
+  // When the user clicks on an empty square, this clicks all surrounding squares
   revealEmpty(indices){
     const squares = this.state.squares.slice();
-    if(squares[indices[0]][indices[1]].value === null) {
-      for(let i = indices[0] - 1; i <= indices[0] + 1; i++){
-        for(let j = indices[1] - 1; j <= indices[1] + 1; j++){
-          if(i >= 0 && i < this.state.height &&
-            j >= 0 && j < this.state.width &&
-            squares[i][j].className === "square"){
-            this.handleClick(this.convert2Dto1D([i,j]))
-          }
+    for(let i = indices[0] - 1; i <= indices[0] + 1; i++){
+      for(let j = indices[1] - 1; j <= indices[1] + 1; j++){
+        if(i >= 0 && i < this.state.height &&
+          j >= 0 && j < this.state.width &&
+          squares[i][j].className === "square"){
+          this.handleClick(this.convert2Dto1D([i,j]),false)
         }
       }
     }
   }
 
-  checkSquaresRemaining(){
-    const squares = this.state.squares.slice();
+  // This checks for and returns the number of remaining squares to reveal
+  checkSquaresRemaining(squares){
     let revealedCount = 0;
     for(let i = 0; i < this.state.height; i++){
       for(let j = 0; j < this.state.width; j++){
@@ -376,6 +402,8 @@ class Game extends React.Component {
     return revealedCount;
   }
 
+  // Upon losing the game, this reveals all remaining mines by adding " exploded"
+  // to the class names of all the unrevealed and unflagged squares
   revealAllMines(){
     const squares = this.state.squares.slice();
     for(let i = 0; i < this.state.height; i++){
@@ -390,7 +418,65 @@ class Game extends React.Component {
     })
   }
 
-  handleSolveClick(){
+  winLoss(indices) {
+    // In case of loss
+    if(this.state.squares[indices[0]][indices[1]].value === "M") {
+      this.stoptimer();
+      this.setState({
+        gameOver: true,
+      })
+      this.revealAllMines();
+      return;
+    }
+
+    // In case of win
+    if(this.state.squaresRemaining === 0){
+      this.stopTimer();
+      this.setState({
+        gameOver: true,
+      })
+      return;
+    }
+
+  }
+
+  isBorder(row,col) {
+    const squares = this.state.squares.slice();
+    // if(squares[row][col].className !== "square revealed") {
+    //   return false;
+    // }
+    // console.log(squares);
+    for(let i = row - 1; i <=  row + 1; i++) {
+      for(let j = col - 1; j <= col + 1; j++) {
+        if(i >= 0 && i < this.state.height &&
+          j >= 0 && j < this.state.width &&
+          !(i === row && j === col)){
+          // console.log('hello');
+          if(!squares[i][j].flagged && squares[i][j].className === "square") {
+            return true;
+          }
+          
+        }
+      }
+    }
+    return false;
+  }
+
+  cullBorder(arr) {
+    let resultArr = [];
+    console.log(arr);
+    for(let i = 0; i < arr.length; i++) {
+      // console.log(arr[i]);
+      // console.log(this.isBorder(arr[i][0],arr[i][1]));
+      if(this.isBorder(arr[i][0],arr[i][1])) {
+        resultArr.push(arr[i]);
+      }
+    }
+    console.log(resultArr);
+    return resultArr;
+  }
+
+  handleSolveClick() {
 
   }
 
@@ -408,7 +494,8 @@ class Game extends React.Component {
       <div className="game">
         <div className={"around-board " + this.state.difficulty}>
           <p>{"Time elapsed: " + this.state.time}</p>
-          <SelectDifficulty onChange={this.handleDifficultyChange}/>
+          <SelectDifficulty onChange={this.handleDifficultyChange}
+            className="difficulty"/>
           <div>
             <NewGame onClick={this.handleNewGameClick}
               className={smileButton}/>
@@ -416,13 +503,17 @@ class Game extends React.Component {
           <div className = "game-board">
             <Board
             	squares = {this.state.squares}
-            	onClick = {(i) =>  this.handleClick(i)}
+            	onClick = {(i) =>  this.handleClick(i,true)}
               onContextMenu = {(event, i) => this.handleContextMenu(event, i)}
               height = {this.state.height}
               width = {this.state.width}
               numMines = {this.state.numMines}
               convert1Dto2D = {this.convert1Dto2D}
             />
+          </div>
+          <div>
+            <SolveButton onClick={this.handleSolveClick}
+              className="solve"/>
           </div>
           <p>
             {this.state.squaresRemaining === 0 ? "Great Job!!" : 
